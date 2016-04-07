e44b21993188f363af6da3f01f26c0ab1055aef3
Bowen Yu <yubowenok@gmail.com>
2016-04-07 11:54:17 -0400

    + partition.py file

---
+++ partition.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import random
+
+dir = sys.argv[1]
+filepath = sys.argv[2]
+basename = os.path.basename(filepath)
+
+lines = []
+for line in sys.stdin:
+  lines.append(line.strip())
+
+random.seed('mapreduce')
+random.shuffle(lines)
+
+p1 = open(dir + 'part00000-' + basename, 'w')
+p2 = open(dir + 'part00001-' + basename, 'w')
+
+cutoff = len(lines) / 2
+for index, line in enumerate(lines):
+  if index < cutoff:
+    p1.write('%s\n' % line)
+  else:
+    p2.write('%s\n' % line)
+
+p1.close()
+p2.close()
+
